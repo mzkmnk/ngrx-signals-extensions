b826cb6611a5f317c23795d227f0ef47afbddee9
mzkmnk <sma0729mex@gmail.com>
2024-12-07 19:37:56 +0900

    chore:init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import type { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'with-storage-sync',
+    loadComponent:() => import('./pages/with-storage-sync/with-storage-sync.component').then((M) => M.WithStorageSyncComponent),
+  },
+  {
+    path:'**',
+    redirectTo:'with-storage-sync',
+  }
+];
---
+++ src/app/pages/with-storage-sync/with-storage-sync.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-with-storage-sync',
+  template:`
+  <h1>With Storage Sync</h1>
+`
+})
+export class WithStorageSyncComponent {
+}
